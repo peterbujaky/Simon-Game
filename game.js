328dc7937552e4a7857dd21a12aac5c2bd468018
peterbujaky <104648835+peterbujaky@users.noreply.github.com>
2022-12-27 19:50:27 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,12 +1,13 @@
+const audio = new Audio();
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = new Array();
-const audio = new Audio();
 const buttons = $(".btn").get();
 let userClickedPattern = new Array();
 let firstKeyPress = true;
 let level = 0;
 let started = false;
 let nextSequenceInProgress = false;
+const isMobile = window.matchMedia("(max-width: 767px)").matches;
 
 /* FUNCTIONS */
 
@@ -131,3 +132,84 @@ buttons.forEach(item => {
 
     });
 });
+
+if (isMobile) {
+
+    $("h1").text("Touch The Scren To Start");
+
+    // CHECK ANSWER FUNCTION 
+
+    $(document).on("touchstart", ((e) => {
+        if (firstKeyPress) {
+            nextSequence();
+            firstKeyPress = false;
+        }
+
+        $("h1").text(`Level ${level}`);
+    }))
+
+
+    // NEXT ROUND
+    function nextSequence() {
+
+        if (nextSequenceInProgress) {
+            return;
+        }
+
+        nextSequenceInProgress = true;
+
+        let randomNumber = Math.floor(Math.random() * 4);
+        let randomChosenColor = buttonColors[randomNumber];
+        gamePattern.push(randomChosenColor);
+        playSound(randomChosenColor);
+        flashEffect(randomChosenColor);
+        level++;
+        $("h1").text(`Level ${level}`)
+        userClickedPattern = [];
+
+        nextSequenceInProgress = false;
+    }
+
+
+
+
+    buttons.forEach(item => {
+        item.addEventListener("touchstart", (e) => {
+
+            /* PRESS EFFECT */
+            animatePress(e.target);
+
+            /* RECOGNIZES THE BUTTON'S COLOR AND STORES IT IN A VARIABLE */
+            let userChosenColor = $(e.target).attr('id');
+            userClickedPattern.push(userChosenColor);
+
+            /* PLAYS AUDIO */
+            playSound(userChosenColor);
+
+            checkAnswer();
+
+            let isMatch = checkAnswer(level);
+            if (isMatch && userClickedPattern.length === gamePattern.length) {
+                setTimeout(() => {
+                    nextSequence()
+                }, 1000);
+                userClickedPattern = [];
+            } else if (!isMatch) {
+                $("h1").text("Game Over, Touch the Screen to Start");
+                $("body").addClass("game-over");
+                setTimeout(() => {
+                    $("body").removeClass("game-over")
+                }, 200)
+                audio.src = "sounds/wrong.mp3";
+                audio.play();
+                level = 0;
+                $(document).on.apply("touchsart", (() => {
+                    startOver();
+                }))
+            }
+
+
+        });
+    });
+
+}
